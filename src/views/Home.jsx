eeabf5fb0afab1b8d5d93d5b0201c88a5a86eaeb
Marco Gallardo <marco@linkmg.cl>
2022-11-28 16:18:44 -0300

    tercer commit

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,7 +4,7 @@ export default () => {
   return (
     <Container className="text-center">
       <h1 className="pt-5">
-        Biendenido a <span className="fw-bold">Happy Cake</span> 
+        Bienvenido a <span className="fw-bold">Happy Cake</span> 
       </h1>
       <h6> El lugar de los pasteles felices </h6>
       <h1>🎂</h1>
